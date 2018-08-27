fcf91317dc82c326384b5da0392f124b2da23e3b
xuelong.chen <xuelong.chen@goodbaby.com>
2018-08-27 12:40:26 +0800

    rm environments

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  SERVER_URL: `./`,
-  production: true,
-  useHash: true,
-  hmr: false,
-  app_key: '1001',
-  gateway:'http://cmanage.gb246.com/omp_cmanage/gateway',
-};
